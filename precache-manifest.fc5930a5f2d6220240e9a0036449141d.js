063f559cb0bcc56e964bd4dbd2aea88ab880460e
Viktors Jenovs <jviktors@gmail.com>
2020-02-29 13:57:23 +0200

    Updates

---
+++ precache-manifest.fc5930a5f2d6220240e9a0036449141d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b62c7ee1bb8df297b4b08a00bf4382",
+    "url": "/picker/index.html"
+  },
+  {
+    "revision": "a44ddbf63575980e7f89",
+    "url": "/picker/static/css/main.3ba17242.chunk.css"
+  },
+  {
+    "revision": "976f7ce3693ef404ab2d",
+    "url": "/picker/static/js/2.6191c934.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/picker/static/js/2.6191c934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a44ddbf63575980e7f89",
+    "url": "/picker/static/js/main.5ef04119.chunk.js"
+  },
+  {
+    "revision": "0dd9a2e3c82d05a47d21",
+    "url": "/picker/static/js/runtime-main.ba73108f.js"
+  }
+]);
